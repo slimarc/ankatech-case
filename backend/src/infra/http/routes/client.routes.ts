8dce62ad02f7fe0333c51ad8cfc22cacf168b8e6
Ricardo Lima <sricardolimaa@gmail.com>
2025-06-01 03:10:46 -0300

    feat: configure routing for clients

---
+++ backend/src/infra/http/routes/client.routes.ts
@@ -0,0 +1,34 @@
+import { FastifyInstance } from 'fastify'
+import { ClientController } from '../controllers/client.controller'
+
+export const clientRoutes = (fastify: FastifyInstance, controller: ClientController) => {
+    fastify.route({
+        method: 'POST',
+        url: '/clients',
+        handler: controller.create.bind(controller),
+    })
+
+    fastify.route({
+        method: 'GET',
+        url: '/clients/:id',
+        handler: controller.findById.bind(controller),
+    })
+
+    fastify.route({
+        method: 'GET',
+        url: '/clients',
+        handler: controller.findMany.bind(controller),
+    })
+
+    fastify.route({
+        method: 'PATCH',
+        url: '/clients/:id',
+        handler: controller.update.bind(controller),
+    })
+
+    fastify.route({
+        method: 'DELETE',
+        url: '/clients/:id',
+        handler: controller.delete.bind(controller),
+    })
+}
