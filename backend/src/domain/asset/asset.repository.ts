07933bbf2ffcd8b7f12ed4e3fc96dcead5518ffe
Ricardo Lima <sricardolimaa@gmail.com>
2025-06-01 23:18:21 -0300

    refactor: refactoring database and adding portfolio

--- backend/src/domain/asset/asset.repository.ts
+++ backend/src/domain/asset/asset.repository.ts
@@ -9,9 +9,14 @@ import {
 
 export interface AssetRepository {
     create(data: CreateAsset): Promise<AssetResponse>
-    update(id: string, data: UpdateAsset): Promise<AssetResponse>
-    delete(params: DeleteAsset): Promise<void>
+
     findById(params: FindAsset): Promise<AssetResponse>
+
     findMany(params: FindAssets): Promise<AssetsResponse>
+
     findManyByClientId(params: FindAssetsByClient): Promise<AssetsByClientResponse>
+
+    update(id: string, data: UpdateAsset): Promise<AssetResponse>
+
+    delete(params: DeleteAsset): Promise<void>
 }
