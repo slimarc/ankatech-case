7f3c7cd0c99028ec21db69f9a2e378a4e3cb63c2
Ricardo Lima <sricardolimaa@gmail.com>
2025-06-02 00:36:53 -0300

    feat: creating findfeat: add find and list schemas for portfolio

--- backend/src/domain/portfolio/portfolio.schemas.ts
+++ backend/src/domain/portfolio/portfolio.schemas.ts
@@ -6,6 +6,16 @@ export const createPortfolioSchema = z.object({
     quantity: z.number().positive()
 })
 
+export const findPortfolioSchema = z.object({
+    id: z.string().uuid()
+})
+
+export const findPortfoliosSchema = z.object({
+    page: z.number().int().positive().optional().default(1),
+    limit: z.number().int().positive().optional().default(10),
+    search: z.string().optional()
+})
+
 export const portfolioResponseSchema = z.object({
     id: z.string().uuid(),
     clientId: z.string().uuid(),
@@ -13,5 +23,8 @@ export const portfolioResponseSchema = z.object({
     quantity: z.number(),
 })
 
+
 export type CreatePortfolio = z.infer<typeof createPortfolioSchema>
 export type PortfolioResponse = z.infer<typeof portfolioResponseSchema>
+export type FindPortfolio = z.infer<typeof findPortfolioSchema>
+export type FindPortfolios = z.infer<typeof findPortfoliosSchema>
