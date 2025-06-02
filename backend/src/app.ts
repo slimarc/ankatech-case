9f2c2ec21f24b222db74a744062ac0be73567b11
Ricardo Lima <sricardolimaa@gmail.com>
2025-06-02 00:17:47 -0300

    refactor: remove assetsCrontrolle in clientRoute

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,15 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
-import { PrismaClientRepository } from './infra/prisma/prisma.client.repository'
+import { AssetService } from './application/services/asset.services'
+import { AssetController } from '@infra/http/controllers/asset.controller'
+import { PrismaAssetRepository } from '@infra/prisma/prisma.asset.repository'
+import { PrismaClientRepository } from '@infra/prisma/prisma.client.repository'
 import { ClientService } from './application/services/client.services'
-import { ClientController } from './infra/http/controllers/client.controller'
-import { clientRoutes } from './infra/http/routes/client.routes'
-import {errorHandler} from "./infra/http/middleware/error-handler";
+import { ClientController } from '@infra/http/controllers/client.controller'
+import { assetRoutes } from '@infra/http/routes/asset.routes'
+import { clientRoutes } from '@infra/http/routes/client.routes'
+import { errorHandler } from "@infra/http/middleware/error-handler";
 
 
 export function buildApp() {
@@ -13,12 +17,17 @@ export function buildApp() {
 
     app.register(cors, { origin: true })
 
-    const prisma = new PrismaClient()
-    const clientRepository = new PrismaClientRepository(prisma)
+    const prismaClient = new PrismaClient()
+    const clientRepository = new PrismaClientRepository(prismaClient)
     const clientService = new ClientService(clientRepository)
     const clientController = new ClientController(clientService)
 
+    const assetRepository = new PrismaAssetRepository(prismaClient)
+    const assetService = new AssetService(assetRepository)
+    const assetController = new AssetController(assetService)
+
     app.register((fastify, _opts, done) => {
+        assetRoutes(fastify, assetController)
         clientRoutes(fastify, clientController)
         done()
     })
