e7be5aeb21ac0d507d03a0c4c78020e5f3c30600
Ricardo Lima <sricardolimaa@gmail.com>
2025-06-02 00:12:49 -0300

    refactor: remove clientId

--- backend/src/domain/asset/asset.schemas.ts
+++ backend/src/domain/asset/asset.schemas.ts
@@ -1,14 +1,13 @@
 import {z} from 'zod'
 
 export const createAssetSchema = z.object({
-    name: z.string().min(1),
-    value: z.number().positive(),
-    clientId: z.string().uuid()
+    name: z.string().min(1).max(255),
+    currentValue: z.number().positive(),
 })
 
 export const updateAssetSchema = z.object({
-    name: z.string().min(1).optional(),
-    value: z.number().positive().optional(),
+    name: z.string().min(1).max(255).optional(),
+    currentValue: z.number().positive().optional(),
 })
 
 export const findAssetSchema = z.object({
@@ -21,13 +20,6 @@ export const findAssetsSchema = z.object({
     search: z.string().optional()
 })
 
-export const findAssetsByClientSchema = z.object({
-    page: z.number().int().positive().optional().default(1),
-    limit: z.number().int().positive().optional().default(10),
-    clientId: z.string().uuid(),
-    search: z.string().optional()
-})
-
 export const deleteAssetSchema = z.object({
     id: z.string().uuid()
 })
@@ -35,8 +27,7 @@ export const deleteAssetSchema = z.object({
 export const assetResponseSchema = z.object({
     id: z.string().uuid(),
     name: z.string(),
-    value: z.number(),
-    clientId: z.string().uuid()
+    currentValue: z.number(),
 })
 
 export const assetsResponseSchema = z.object({
@@ -46,18 +37,10 @@ export const assetsResponseSchema = z.object({
     limit: z.number()
 })
 
-export const assetsByClientResponseSchema = z.object({
-    assets: z.array(assetResponseSchema),
-    total: z.number(),
-    page: z.number(),
-})
-
 export type CreateAsset = z.infer<typeof createAssetSchema>
 export type UpdateAsset = z.infer<typeof updateAssetSchema>
-export type AssetResponse = z.infer<typeof assetResponseSchema>
 export type FindAsset = z.infer<typeof findAssetSchema>
 export type FindAssets = z.infer<typeof findAssetsSchema>
-export type FindAssetsByClient = z.infer<typeof findAssetsByClientSchema>
 export type DeleteAsset = z.infer<typeof deleteAssetSchema>
+export type AssetResponse = z.infer<typeof assetResponseSchema>
 export type AssetsResponse = z.infer<typeof assetsResponseSchema>
-export type AssetsByClientResponse = z.infer<typeof assetsByClientResponseSchema>
