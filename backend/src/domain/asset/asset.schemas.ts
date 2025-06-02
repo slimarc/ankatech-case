c5cfb3f6674fa8dd6066201b8e9126ae92635f16
Ricardo Lima <sricardolimaa@gmail.com>
2025-06-02 19:17:09 -0300

    refactor: refactoring current value to string

--- backend/src/domain/asset/asset.schemas.ts
+++ backend/src/domain/asset/asset.schemas.ts
@@ -1,13 +1,21 @@
 import {z} from 'zod'
+import { Decimal } from '@prisma/client/runtime/library';
+
+export const currencyStringInputSchema = z.string().nonempty()
+    .regex(/^\d+(\.\d{1,2})?$/, "Invalid value format.")
+    .transform(valStr => new Decimal(valStr))
+    .refine(decimalVal => decimalVal.isPositive() && !decimalVal.isZero(), {
+        message: "The current value must be a positive number and greater than zero."
+    });
 
 export const createAssetSchema = z.object({
     name: z.string().min(1).max(255),
-    currentValue: z.number().positive(),
+    currentValue: currencyStringInputSchema
 })
 
 export const updateAssetSchema = z.object({
     name: z.string().min(1).max(255).optional(),
-    currentValue: z.number().positive().optional(),
+    currentValue: currencyStringInputSchema.optional()
 })
 
 export const findAssetSchema = z.object({
@@ -17,17 +25,24 @@ export const findAssetSchema = z.object({
 export const findAssetsSchema = z.object({
     page: z.number().int().positive().optional().default(1),
     limit: z.number().int().positive().optional().default(10),
-    search: z.string().optional()
+    search: z.string().optional(),
+    minValue: currencyStringInputSchema.optional(),
+    maxValue: currencyStringInputSchema.optional()
 })
 
 export const deleteAssetSchema = z.object({
     id: z.string().uuid()
 })
 
+const decimalOutputSchema = z.custom<Decimal>(
+    (val) => val instanceof Decimal,
+    "Invalid Decimal Value for Response")
+    .refine(d => !d.isNaN() && d.isFinite(), "Decimal of answer must be a finite number");
+
 export const assetResponseSchema = z.object({
     id: z.string().uuid(),
     name: z.string(),
-    currentValue: z.number(),
+    currentValue: decimalOutputSchema.transform(decimalVal => decimalVal.toFixed(2)),
 })
 
 export const assetsResponseSchema = z.object({
