ee1d6edbd5167c4c90fc440577ce5d3189020ac1
Ricardo Lima <sricardolimaa@gmail.com>
2025-06-02 19:15:12 -0300

    refactor: creating migration

---
+++ backend/prisma/migrations/20250602195316_create_client_asset_and_portafolio_and_assetholding/migration.sql
@@ -0,0 +1,49 @@
+-- CreateTable
+CREATE TABLE `clients` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `status` ENUM('ACTIVE', 'INACTIVE') NOT NULL DEFAULT 'ACTIVE',
+
+    UNIQUE INDEX `clients_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `portfolios` (
+    `id` VARCHAR(191) NOT NULL,
+    `clientId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `portfolios_clientId_key`(`clientId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `portfolio_assets` (
+    `id` VARCHAR(191) NOT NULL,
+    `portfolioId` VARCHAR(191) NOT NULL,
+    `assetId` VARCHAR(191) NOT NULL,
+    `quantity` DECIMAL(20, 4) NOT NULL,
+
+    UNIQUE INDEX `portfolio_assets_portfolioId_assetId_key`(`portfolioId`, `assetId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `assets` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `currentValue` DECIMAL(30, 2) NOT NULL,
+
+    UNIQUE INDEX `assets_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `portfolios` ADD CONSTRAINT `portfolios_clientId_fkey` FOREIGN KEY (`clientId`) REFERENCES `clients`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `portfolio_assets` ADD CONSTRAINT `portfolio_assets_portfolioId_fkey` FOREIGN KEY (`portfolioId`) REFERENCES `portfolios`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `portfolio_assets` ADD CONSTRAINT `portfolio_assets_assetId_fkey` FOREIGN KEY (`assetId`) REFERENCES `assets`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
