211da35334f8bc507b00df9209e81a4df155b52c
Ricardo Lima <sricardolimaa@gmail.com>
2025-06-02 00:42:12 -0300

    feat: adding find portfolio by id client

--- backend/src/domain/portfolio/portfolio.schemas.ts
+++ backend/src/domain/portfolio/portfolio.schemas.ts
@@ -10,6 +10,10 @@ export const findPortfolioSchema = z.object({
     id: z.string().uuid()
 })
 
+export const findPortfoliosByClientIdSchema = z.object({
+    clientId: z.string().uuid()
+})
+
 export const findPortfoliosSchema = z.object({
     page: z.number().int().positive().optional().default(1),
     limit: z.number().int().positive().optional().default(10),
@@ -28,3 +32,4 @@ export type CreatePortfolio = z.infer<typeof createPortfolioSchema>
 export type PortfolioResponse = z.infer<typeof portfolioResponseSchema>
 export type FindPortfolio = z.infer<typeof findPortfolioSchema>
 export type FindPortfolios = z.infer<typeof findPortfoliosSchema>
+export type FindPortfoliosByClientId = z.infer<typeof findPortfoliosByClientIdSchema>
