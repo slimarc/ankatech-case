57684c40ccca7ca68073777383e7a6971677d85c
Ricardo Lima <sricardolimaa@gmail.com>
2025-06-03 00:38:04 -0300

    feat: create asset holding repository

---
+++ backend/src/domain/asset-holding/asset-holding.repository.ts
@@ -0,0 +1,21 @@
+import {
+    CreateAssetHolding,
+    DeleteAssetHolding,
+    FindAssetHoldingById,
+    FindAssetHoldingsByPortfolioId,
+    UpdateAssetHoldingQuantity,
+    AssetHoldingResponse,
+    AssetHoldingsResponse
+} from './asset-holding.schemas';
+
+export interface AssetHoldingRepository {
+    create(data: CreateAssetHolding): Promise<AssetHoldingResponse>;
+
+    findById(params: FindAssetHoldingById): Promise<AssetHoldingResponse | null>;
+
+    findByPortfolioId(params: FindAssetHoldingsByPortfolioId): Promise<AssetHoldingsResponse>;
+
+    updateQuantity(params: UpdateAssetHoldingQuantity): Promise<AssetHoldingResponse>;
+
+    delete(params: DeleteAssetHolding): Promise<void>;
+}
