992d8b671a04b18eb2beaa94aa2c4d5c72e0d4f1
Ricardo Lima <sricardolimaa@gmail.com>
2025-06-03 00:01:14 -0300

    feat: adding portfolio instance

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,11 +5,15 @@ import { AssetService } from './application/services/asset.services'
 import { AssetController } from '@infra/http/controllers/asset.controller'
 import { PrismaAssetRepository } from '@infra/prisma/prisma.asset.repository'
 import { PrismaClientRepository } from '@infra/prisma/prisma.client.repository'
+import { PrismaPortfolioRepository } from '@infra/prisma/prisma.portfolio.repository'
 import { ClientService } from './application/services/client.services'
 import { ClientController } from '@infra/http/controllers/client.controller'
 import { assetRoutes } from '@infra/http/routes/asset.routes'
 import { clientRoutes } from '@infra/http/routes/client.routes'
+import { portfolioRoutes } from "@infra/http/routes/portfolio.routes";
 import { errorHandler } from "@infra/http/middleware/error-handler";
+import {PortfolioService} from "@application/services/portfolio.services";
+import {PortfolioController} from "@infra/http/controllers/portfolio.controller";
 
 
 export function buildApp() {
@@ -19,14 +23,20 @@ export function buildApp() {
 
     const prismaClient = new PrismaClient()
     const clientRepository = new PrismaClientRepository(prismaClient)
-    const clientService = new ClientService(clientRepository)
+    const portfolioRepository = new PrismaPortfolioRepository(prismaClient)
+
+    const clientService = new ClientService(clientRepository, portfolioRepository)
     const clientController = new ClientController(clientService)
 
     const assetRepository = new PrismaAssetRepository(prismaClient)
     const assetService = new AssetService(assetRepository)
     const assetController = new AssetController(assetService)
 
+    const portfolioService = new PortfolioService(portfolioRepository)
+    const portfolioController = new PortfolioController(portfolioService)
+
     app.register((fastify, _opts, done) => {
+        portfolioRoutes(fastify, portfolioController)
         assetRoutes(fastify, assetController)
         clientRoutes(fastify, clientController)
         done()
