459f553041be7fb5fffe8428875dfb94694429b2
Ricardo Lima <sricardolimaa@gmail.com>
2025-06-05 17:17:19 -0300

    feat: creating hook to delete users

--- frontend/src/hooks/useRemoveClient.ts
+++ frontend/src/hooks/useRemoveClient.ts
@@ -1,17 +1,47 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import {ClientApiService} from "@/services/clientService";
+import { ClientApiService } from "@/services/clientService";
+import React from "react";
+
+interface AlertState {
+    isVisible: boolean;
+    variant: "default" | "destructive";
+    title: string;
+    description: string;
+}
 
 export const useRemoveClient = () => {
     const queryClient = useQueryClient();
 
-    return useMutation({
+    const [alertInfo, setAlertInfo] = React.useState<AlertState>({
+        isVisible: false,
+        variant: "default",
+        title: "",
+        description: "",
+    });
+
+    const mutation = useMutation({
         mutationFn: (clientId: string) => ClientApiService.remove(clientId),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey : ['clients']});
+            setAlertInfo({
+                isVisible: true,
+                variant: "default",
+                title: "Sucesso!",
+                description: "Cliente excluído com sucesso."
+            });
+            setTimeout(() => setAlertInfo(prevAlertInfo => ({ ...prevAlertInfo, isVisible: false })), 3000);
         },
-        onError: (err) => {
-            console.log('Error removing client',err);
-            alert('Error removing client' + err.message);
+        onError: (err: any) => {
+            console.error('Erro ao excluir cliente:', err);
+            setAlertInfo({
+                isVisible: true,
+                variant: "destructive",
+                title: "Erro na Exclusão",
+                description: `Não foi possível excluir o cliente: ${err.message || 'Erro desconhecido'}`
+            });
+            setTimeout(() => setAlertInfo(prevAlertInfo => ({ ...prevAlertInfo, isVisible: false })), 5000);
         }
     });
+
+    return { ...mutation, alertInfo };
 }
